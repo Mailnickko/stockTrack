0325758dd8e2cd99f336a0713ae4e89209abf56f
Nicholas Ko <mailnickko@gmail.com>
2016-07-11 13:05:41 -0700

    fixed port for heroku

--- server/server.js
+++ server/server.js
@@ -8,6 +8,12 @@ mongoose.connect('mongodb://localhost/stockTrack');
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
+var port = process.env.PORT || 3000;
+
 app.listen(3000);
+
+app.listen(port, function() {
+  console.log('connected!')
+})
 
 module.exports = app;
