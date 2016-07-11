bbd4eb1a659daa869b06ae305c807ef9f5fc1bf7
Nicholas Ko <mailnickko@gmail.com>
2016-07-11 13:11:21 -0700

    updates mongoose connect for heroku

--- server/server.js
+++ server/server.js
@@ -8,12 +8,23 @@ mongoose.connect('mongodb://localhost/stockTrack');
 require('./config/middleware.js')(app, express);
 require('./config/routes.js')(app, express);
 
-var port = process.env.PORT || 3000;
+var uristring =
+    process.env.MONGOLAB_URI ||
+    process.env.MONGOHQ_URL ||
+    'mongodb://localhost/stockTrack';
 
-app.listen(3000);
+var port = process.env.PORT || 3000;
 
 app.listen(port, function() {
   console.log('connected!')
 })
+
+mongoose.connect(uristring, function (err, res) {
+      if (err) {
+      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+      } else {
+      console.log ('Succeeded connected to: ' + uristring);
+      }
+    });
 
 module.exports = app;
