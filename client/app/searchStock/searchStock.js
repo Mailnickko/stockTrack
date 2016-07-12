c823c2ec6258f76700fe311e4850de0118d36ddd
Nicholas Ko <mailnickko@gmail.com>
2016-07-11 19:14:55 -0700

    fix for angular mixed content issue

--- client/app/searchStock/searchStock.js
+++ client/app/searchStock/searchStock.js
@@ -1,16 +1,24 @@
 angular.module('stockTrack.search', [])
 
 .controller('searchController', function($scope, Stock) {
-  $scope.searchStock = function() {
-    if ($scope.searchQuery === null || $scope.searchQuery === '') {
-      return false;
-    } else {
-      var promise = Stock.searchStock($scope.searchQuery);
 
-        promise.then(function(result) {
+
+  $scope.searchStock = function() {
+    $scope.showErr = false;
+    $scope.noneFound = false;
+    if (!$scope.searchQuery) {
+      $scope.showErr = true;
+      return;
+    } else {
+      Stock.searchStock($scope.searchQuery)
+      .then(function(result) {
           $scope.searchResults = result.data;
-          $scope.showTable = true;
-          console.log($scope.searchResults);
+          if (!$scope.searchResults.length) {
+            $scope.noneFound = true;
+          } else {
+            $scope.showTable = true;
+            console.log($scope.searchResults)
+          }
         })
         .catch(function(err) {
           console.log('Error in controller: ' + err);
